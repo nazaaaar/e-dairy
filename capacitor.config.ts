0cd87acf5959bfb14ef35b5bf6c8ad1742606fbe
Nazarii Kurei <94959470+nazaaaar@users.noreply.github.com>
2024-04-05 11:59:34 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'E-dairy',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
